8611610c83bc52511199a1c8f23ebf4c83abb77b
Enrique Lopez Magallon <enadol@yahoo.com>
2023-07-02 10:37:11 +0200

    Update 2widgets-streamlit.py

--- 2widgets-streamlit.py
+++ 2widgets-streamlit.py
@@ -46,7 +46,8 @@ dropdown_club = st.selectbox(label="Seleccione el club: ", options =    unique_s
 
 dropdown_player = st.selectbox(label="Seleccione el Jugador: ", options =    unique_sorted_values_plus_ALL(df.Jugador))
 
-st.sidebar([dropdown_club, dropdown_player])
+st.sidebar(dropdown_club)
+st.sidebar(dropdown_player)
 
 # In[45]:
 
