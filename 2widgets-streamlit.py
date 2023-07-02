bae9788a6d6e359bbf4b270a61bb14230b9de651
Enrique Lopez Magallon <enadol@yahoo.com>
2023-07-02 15:55:01 +0200

    Update 2widgets-streamlit.py

--- 2widgets-streamlit.py
+++ 2widgets-streamlit.py
@@ -57,7 +57,7 @@ def common_filtering(dropdown_club, dropdown_player):
         return common_filter
 
 #dropdown_club
-dropdown_club = st.sidebar.selectbox(label="Seleccione el club: ", options =    unique_sorted_values_plus_ALL(df.Club), on_change=selectOptions(dropdown_club))
+dropdown_club = st.sidebar.selectbox(label="Seleccione el club: ", options =    unique_sorted_values_plus_ALL(df.Club), on_change=df.filter(df.Club==dropdown_club))
 dropdown_player = st.sidebar.selectbox(label="Seleccione el Jugador: ", options =    unique_sorted_values_plus_ALL(df.Jugador))
 
 def selectOptions(club):
