4bb6919cb5b8ead902b4077b67833aafc5245bb5
Enrique Lopez Magallon <enadol@yahoo.com>
2023-07-05 09:07:51 +0200

    Update 2widgets-streamlit.py

--- 2widgets-streamlit.py
+++ 2widgets-streamlit.py
@@ -24,21 +24,16 @@ st.set_page_config(layout = "wide", page_title="Bundesliga Data Dashboard - Torn
 df=pd.read_excel("blPlayersAll23.xlsm", sheet_name="blplayers2023")
 
 
-cell_hover = {
-    "selector": "td:hover",
-    "props": [("background-color", "#FFFFE0")]
-}
-index_names = {
-    "selector": ".index_name",
-    "props": "font-style: italic; color: darkgrey; font-weight:normal;"
-}
 headers = {
     "selector": "th:not(.index_name)",
-    "props": "background-color: #800000; color: white;"
+    "props": "background-color: #800000; color: white; text-align: center"
 }
+properties = {"border": "1px solid black", "width": "65px", "text-align": "center"}
+
+df.style.format(precision=2).set_table_styles([cell_hover, index_names, headers]).set_properties(**properties)
 
 
-df.style.set_table_styles([cell_hover, index_names, headers])
+#df.style.set_table_styles([cell_hover, index_names, headers])
 
 ALL = 'TODOS'
 def unique_sorted_values_plus_ALL(array):
@@ -95,7 +90,7 @@ with st.container():
     if dropdown_club == ALL:
         if dropdown_player==ALL:
             st.write(f'Indicadores Club: {dropdown_club} - Jugador: {dropdown_player}')
-            st.write(df.style.set_table_styles([cell_hover, index_names, headers]))
+            st.write(df)
         else:
             st.write(f'Indicadores Club: {dropdown_club} - Jugador: {dropdown_player}')
             st.write(df[df['Jugador']==dropdown_player])
