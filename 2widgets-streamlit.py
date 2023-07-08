e7490fa7b8959f2618758660bfd205d404e2ce9f
Enrique Lopez Magallon <enadol@yahoo.com>
2023-07-08 08:56:14 +0200

    Update 2widgets-streamlit.py

--- 2widgets-streamlit.py
+++ 2widgets-streamlit.py
@@ -21,7 +21,7 @@ import plotly as plt
 
 st.set_page_config(layout = "wide", page_title="Bundesliga Data Dashboard - Torneo 2022/2023")
 
-dfpre=pd.read_excel("blPlayersAll23.xlsm", sheet_name="blplayers2023")
+df=pd.read_excel("blPlayersAll23.xlsm", sheet_name="blplayers2023")
 
     
 cell_hover = {
@@ -45,7 +45,7 @@ properties = {"border": "1px solid black", "width": "65px", "text-align": "cente
 
 
 df= st.data_editor(
-dfpre, hide_index=True
+df, hide_index=True
 )
 
 
